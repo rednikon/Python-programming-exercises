d01c05664798aa6ad718274175a10905c1450e43
Vee McClure <rednikon@yahoo.com>
2019-06-11 15:34:48 -0700

    created answer .py file

---
+++ my_answers1-10.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Python Programming Exercises 1 - 10
+
+Created on Tue Jun 11 15:22:52 2019
+
+@author: veemac
+"""
+
+# Question 1: Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+# My answer: use the rang function to find the applicable numbers, and present them as a list (comma-separated on a single line)
